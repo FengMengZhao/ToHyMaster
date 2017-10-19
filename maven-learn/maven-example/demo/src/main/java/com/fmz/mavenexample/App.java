d8313bd67ae05054489167a1fe8cd4e3d2eea330
fengmengzhao <fengmengzhao@thunisoft.com>
2017-10-19 10:43:48 +0800

    update

---
+++ maven-learn/maven-example/demo/src/main/java/com/fmz/mavenexample/App.java
@@ -0,0 +1,13 @@
+package com.fmz.mavenexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
